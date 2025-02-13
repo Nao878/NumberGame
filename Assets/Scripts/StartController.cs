c155761dcc542167515665e8ce7cd338d2358858
Nao878 <yanaoto220@gmail.com>
2025-02-13 16:55:03 +0900

    TemporaryFix1File

--- Assets/Scripts/StartController.cs
+++ Assets/Scripts/StartController.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class StartController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("StartController");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
